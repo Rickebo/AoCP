a5d84ef5befa8df2dcf6e0d9246c6dc7e01e0d3f
Jesper Wahl <jesper.wahl@hotmail.com>
2024-12-06 07:47:35 +0100

    Added day05 & day06

--- backend/Problems/Year2024/Day05.cs
+++ backend/Problems/Year2024/Day05.cs
@@ -14,7 +14,7 @@ public class Day05 : ProblemSet
         new SecondProblem()
     ];
 
-    public override string Name => "Temp";
+    public override string Name => "Print Queue";
 
     public class FirstProblem : Problem
     {
@@ -22,17 +22,32 @@ public class Day05 : ProblemSet
 
         public override string Description { get; } = "";
 
-        public override async Task Solve(string input, Reporter reporter)
+        public override Task Solve(string input, Reporter reporter)
         {
+            Sorter sorter = new();
             string[] rows = Parser.SplitBy(input, ["\r\n", "\r", "\n"]);
+            int res = 0;
+
+            foreach (string row in rows)
+            {
+                if (row.Contains('|'))
+                {
+                    sorter.AddRule(row, reporter);
+                }
+                else
+                {
+                    res += sorter.Update(row, reporter);
+                }
+            }
 
             reporter.Report(
                 new FinishedProblemUpdate()
                 {
                     Successful = true,
-                    Solution = ""
+                    Solution = res.ToString()
                 }
             );
+            return Task.CompletedTask;
         }
     }
 
@@ -42,17 +57,158 @@ public class Day05 : ProblemSet
 
         public override string Description { get; } = "";
 
-        public override async Task Solve(string input, Reporter reporter)
+        public override Task Solve(string input, Reporter reporter)
         {
+            Sorter sorter = new();
             string[] rows = Parser.SplitBy(input, ["\r\n", "\r", "\n"]);
+            int res = 0;
+
+            foreach (string row in rows)
+            {
+                if (row.Contains('|'))
+                {
+                    sorter.AddRule(row, reporter);
+                }
+                else
+                {
+                    res += sorter.UpdateFixer(row, reporter);
+                }
+            }
 
             reporter.Report(
                 new FinishedProblemUpdate()
                 {
                     Successful = true,
-                    Solution = ""
+                    Solution = res.ToString()
                 }
             );
+            return Task.CompletedTask;
+        }
+    }
+
+    private class Sorter
+    {
+        public List<int> printed = [];
+        public Dictionary<int, List<int>> printRules = [];
+
+        public Sorter() { }
+
+        public void AddRule(string row, Reporter reporter)
+        {
+            int[] pages = Parser.GetValues<int>(row);
+            if (!printRules.ContainsKey(pages[1]))
+            {
+                printRules[pages[1]] = [];
+            }
+            if (!printRules[pages[1]].Contains(pages[0]))
+            {
+                printRules[pages[1]].Add(pages[0]);
+            }
+
+            reporter.Report(
+                new TextProblemUpdate()
+                {
+                    Lines = [$"{row} added"]
+                }
+            );
+        }
+
+        public int Update(string row, Reporter reporter)
+        {
+            printed.Clear();
+            int[] pages = Parser.GetValues<int>(row);
+            foreach (int page in pages)
+            {
+                if (printRules.ContainsKey(page))
+                {
+                    foreach (var rule in printRules[page])
+                    {
+                        if (!printed.Contains(rule) && pages.Contains(rule))
+                        {
+                            return 0;
+                        }
+                    }
+                }
+
+                if (!printed.Contains(page))
+                {
+                    printed.Add(page);
+
+                    reporter.Report(
+                        new TextProblemUpdate()
+                        {
+                            Lines = [$"{page} printed"]
+                        }
+                    );
+                }
+            }
+
+            reporter.Report(
+                new TextProblemUpdate()
+                {
+                    Lines = [$"{row} = correct"]
+                }
+            );
+
+            return pages[pages.Length / 2];
+        }
+
+        public int UpdateFixer(string row, Reporter reporter)
+        {
+            printed.Clear();
+            int[] pages = Parser.GetValues<int>(row);
+            Queue<int> queue = new();
+            foreach (int page in pages)
+            {
+                queue.Enqueue(page);
+            }
+
+            bool correct = true;
+            bool skip = false;
+
+            while (queue.TryDequeue(out int page))
+            {
+                skip = false;
+                if (printRules.ContainsKey(page))
+                {
+                    foreach (var rule in printRules[page])
+                    {
+                        if (!printed.Contains(rule) && pages.Contains(rule))
+                        {
+                            queue.Enqueue(page);
+                            correct = false;
+                            skip = true;
+                            break;
+                        }
+                    }
+                }
+
+                if (skip) continue;
+
+                if (!printed.Contains(page))
+                {
+                    printed.Add(page);
+
+                    reporter.Report(
+                        new TextProblemUpdate()
+                        {
+                            Lines = [$"{page} printed"]
+                        }
+                    );
+                }
+            }
+
+            string rep = $"{row} = {(correct ? "correct" : "incorrect")}";
+            string fix = $"Fixed: " + string.Join(",", printed);
+
+            reporter.Report(
+                new TextProblemUpdate()
+                {
+                    Lines = [$"{rep} {(!correct ? fix : "")}"], 
+                }
+            );
+
+            return !correct ? printed[printed.Count / 2] : 0;
         }
     }
 }
--- backend/Problems/Year2024/Day06.cs
+++ backend/Problems/Year2024/Day06.cs
@@ -14,7 +14,7 @@ public class Day06 : ProblemSet
         new SecondProblem()
     ];
 
-    public override string Name => "Temp";
+    public override string Name => "Guard Gallivant";
 
     public class FirstProblem : Problem
     {
@@ -22,17 +22,20 @@ public class Day06 : ProblemSet
 
         public override string Description { get; } = "";
 
-        public override async Task Solve(string input, Reporter reporter)
+        public override Task Solve(string input, Reporter reporter)
         {
-            string[] rows = Parser.SplitBy(input, ["\r\n", "\r", "\n"]);
+            CharGrid grid = new(input);
+
+            MarkWalk(grid, reporter);
 
             reporter.Report(
                 new FinishedProblemUpdate()
                 {
                     Successful = true,
-                    Solution = ""
+                    Solution = WalkedTiles(grid, reporter).ToString()
                 }
             );
+            return Task.CompletedTask;
         }
     }
 
@@ -42,17 +45,185 @@ public class Day06 : ProblemSet
 
         public override string Description { get; } = "";
 
-        public override async Task Solve(string input, Reporter reporter)
+        public override Task Solve(string input, Reporter reporter)
         {
-            string[] rows = Parser.SplitBy(input, ["\r\n", "\r", "\n"]);
+            CharGrid grid = new(input);
 
             reporter.Report(
                 new FinishedProblemUpdate()
                 {
                     Successful = true,
-                    Solution = ""
+                    Solution = LoopCount(grid, reporter).ToString()
                 }
             );
+            return Task.CompletedTask;
         }
     }
+
+    public static Guard GetGuard(CharGrid grid, Reporter reporter)
+    {
+        for (int y = 0; y < grid.Height; y++)
+        {
+            for (int x = 0; x < grid.Width; x++)
+            {
+                foreach (Direction dir in DirectionExtensions.Cardinals)
+                {
+                    if (grid[x, y] == dir.Arrow())
+                    {
+                        return new Guard(new Coordinate<int>(x, y), dir);
+                    }
+                }
+            }
+        }
+        throw new NotImplementedException();
+    }
+
+    public static Guard Walk(Guard guard, CharGrid grid, Reporter reporter)
+    {
+        // Look ahead
+        Coordinate<int> next = guard.pos + Coordinate<int>.UnitY;
+        switch (guard.dir)
+        {
+            case Direction.East:
+                next = guard.pos + Coordinate<int>.UnitX;
+                break;
+            case Direction.South:
+                next = guard.pos - Coordinate<int>.UnitY;
+                break;
+            case Direction.West:
+                next = guard.pos - Coordinate<int>.UnitX;
+                break;
+        }
+
+        // Check if next step is an obstacle
+        if (next.X >= 0 && next.X < grid.Width && next.Y >= 0 && next.Y < grid.Height && grid[next.X, next.Y] == '#')
+        {
+            // Rotate clockwise
+            return new Guard(guard.pos, guard.dir.RotateClockwise());
+        }
+        else
+        {
+            // Keep walking
+            return new Guard(next, guard.dir);
+        }
+    }
+
+    public static void MarkWalk(CharGrid grid, Reporter reporter)
+    {
+        Guard guard = GetGuard(grid, reporter);
+        Coordinate<int> currPos = guard.pos;
+
+        Direction dir = Direction.North;
+        switch(grid[guard.pos.X, guard.pos.Y])
+        {
+            case '>':
+                dir = Direction.East;
+                break;
+            case 'v':
+                dir = Direction.South;
+                break;
+            case '<':
+                dir = Direction.West;
+                break;
+        }
+
+        grid[guard.pos.X, guard.pos.Y] = 'X';
+
+        for (;;)
+        {
+            // Look ahead
+            Coordinate<int> next = currPos + Coordinate<int>.UnitY;
+            switch (dir)
+            {
+                case Direction.East:
+                    next = currPos + Coordinate<int>.UnitX;
+                    break;
+                case Direction.South:
+                    next = currPos - Coordinate<int>.UnitY;
+                    break;
+                case Direction.West:
+                    next = currPos - Coordinate<int>.UnitX;
+                    break;
+            }
+
+            // Check if next step is outside grid
+            if (next.X < 0 || next.X >= grid.Width || next.Y < 0 || next.Y >= grid.Height)
+            {
+                return;
+            }
+
+            // Check if next step is an obstacle
+            if (grid[next.X, next.Y] == '#')
+            {
+                dir = dir.RotateClockwise();
+            }
+            else
+            {
+                // Mark next tile as walked
+                grid[next.X, next.Y] = 'X';
+
+                // Update current position
+                currPos = next;
+            }
+        }
+    }
+
+    public static int WalkedTiles(CharGrid grid, Reporter reporter)
+    {
+        int count = 0;
+        for (int y = 0; y < grid.Height; y++)
+        {
+            for (int x = 0; x < grid.Width; x++)
+            {
+                if (grid[x, y] == 'X')
+                {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    public static int LoopCount(CharGrid grid, Reporter reporter)
+    {
+        int loops = 0;
+        Guard guard = GetGuard(grid, reporter);
+        for (int y = 0; y < grid.Height; y++)
+        {
+            for (int x = 0; x < grid.Width; x++)
+            {
+                if (grid[x, y] == '.')
+                {
+                    grid[x, y] = '#';
+                    if (LoopCheck(grid, guard, reporter))
+                    {
+                        loops++;
+                    }
+                    grid[x, y] = '.';
+                }
+            }
+        }
+        return loops;
+    }
+
+    public static bool LoopCheck(CharGrid grid, Guard guard, Reporter reporter)
+    {
+        HashSet<Guard> visited = [];
+
+        for (;;)
+        {
+            if (guard.pos.X < 0 || guard.pos.X >= grid.Width || guard.pos.Y < 0 || guard.pos.Y >= grid.Height)
+            {
+                return false;
+            }
+            else if (!visited.Add(guard))
+            {
+                return true;
+            }
+
+            guard = Walk(guard, grid, reporter);
+        }
+    }
+
+    public record Guard(Coordinate<int> pos, Direction dir);
 }
