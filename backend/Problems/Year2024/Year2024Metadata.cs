d79e534d655dbd61fed9b511e86ed5d72ac2a8be
Simon Pettersson <rickebo@rickebo.com>
2024-12-07 15:52:42 +0100

    Added 2024

---
+++ backend/Problems/Year2024/Year2024Metadata.cs
@@ -0,0 +1,15 @@
+﻿namespace Backend.Problems.Year2024;
+
+public static class Year2024Metadata
+{
+    public static IServiceCollection AddYear2024(this IServiceCollection services) =>
+        services.AddSingleton<ProblemCollection>(new Year2024Collection());
+
+    public class Year2024Collection : ProblemCollection
+    {
+        public override Dictionary<string, List<ProblemSet>> Problems { get; } =
+            FindProblems(typeof(Year2024Metadata));
+
+        public override int Year { get; } = 2024;
+    }
+}
