db04a5159f30f205e0137e94f43a917d7b74f2b2
Jesper Wahl <jesper.wahl@hotmail.com>
2024-12-08 09:37:51 +0100

    Added option to create grid with default value

--- Lib/Grid/IntGrid.cs
+++ Lib/Grid/IntGrid.cs
@@ -2,21 +2,16 @@
 
 public class IntGrid : ArrayGrid<int>
 {
-    public IntGrid(
-        string input,
-        OriginPosition originPosition = OriginPosition.BottomLeft
-    ) : base(Init(input, originPosition))
-    {
-    }
+    public IntGrid(string input, OriginPosition originPosition = OriginPosition.BottomLeft) : base(ArrFromStr(input, originPosition)) { }
 
-    public IntGrid(int[,] values) : base(values)
-    {
-    }
+    public IntGrid(int num, int width, int height) : base(ArrFromValSize(num, width, height)) { }
 
-    private static int[,] Init(string str, OriginPosition originPosition = OriginPosition.BottomLeft)
+    public IntGrid(int[,] values) : base(values) { }
+
+    private static int[,] ArrFromStr(string str, OriginPosition originPosition = OriginPosition.BottomLeft)
     {
         // Parse string input
-        var rows = Parser.SplitBy(str, ["\r\n", "\r", "\n"]);
+        var rows = str.SplitLines();
         var height = rows.Length;
         var width = rows[0].Length;
 
@@ -29,7 +24,24 @@ public class IntGrid : ArrayGrid<int>
             for (var x = 0; x < width; x++)
             {
                 var mappedY = originPosition == OriginPosition.BottomLeft ? height - 1 - y : y;
-                arr[x, y] = rows[mappedY][x] - '0';
+                arr[x, y] = rows[mappedY][x];
+            }
+        }
+
+        return arr;
+    }
+
+    private static int[,] ArrFromValSize(int num, int width, int height)
+    {
+        // Init array of ints
+        var arr = new int[width, height];
+
+        // Fill 2D-array with ints
+        for (var y = 0; y < height; y++)
+        {
+            for (var x = 0; x < width; x++)
+            {
+                arr[x, y] = num;
             }
         }
 
