55ae69415146e97c6f2ba88cfde0ff32173c5782
Jesper Wahl <jesper.wahl@hotmail.com>
2025-11-22 12:40:28 +0100

    Added dict and clamp extensions

---
+++ src/backend/Lib/NumberExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace Lib
+{
+    public static class NumberExtensions
+    {
+        public static int Clamp(this int value, int min = int.MinValue, int max = int.MaxValue)
+            => Math.Min(Math.Max(value, min), max);
+
+        public static long Clamp(this long value, long min = long.MinValue, long max = long.MaxValue)
+            => Math.Min(Math.Max(value, min), max);
+
+        public static double Clamp(this double value, double min = double.MinValue, double max = double.MaxValue)
+            => Math.Min(Math.Max(value, min), max);
+    }
+}
