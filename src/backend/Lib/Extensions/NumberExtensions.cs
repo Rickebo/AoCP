d70eb6974b0d9d99a2b496f980860565bd6363c8
Jesper Wahl <jesper.wahl@hotmail.com>
2025-11-29 15:53:58 +0100

    Made clamp extension generic

--- src/backend/Lib/Extensions/NumberExtensions.cs
+++ src/backend/Lib/Extensions/NumberExtensions.cs
@@ -1,14 +1,15 @@
-﻿namespace Lib.Extensions
+﻿using System.Numerics;
+
+namespace Lib.Extensions
 {
     public static class NumberExtensions
     {
-        public static int Clamp(this int value, int min = int.MinValue, int max = int.MaxValue)
-            => Math.Min(Math.Max(value, min), max);
+        public static T Clamp<T>(this T value, T min, T max)
+            where T : INumber<T> =>
+            T.Clamp(value, min, max);
 
-        public static long Clamp(this long value, long min = long.MinValue, long max = long.MaxValue)
-            => Math.Min(Math.Max(value, min), max);
-
-        public static double Clamp(this double value, double min = double.MinValue, double max = double.MaxValue)
-            => Math.Min(Math.Max(value, min), max);
+        public static T Clamp<T>(this T value, T? min = null, T? max = null)
+            where T : struct, INumber<T>, IMinMaxValue<T> =>
+            T.Clamp(value, min ?? T.MinValue, max ?? T.MaxValue);
     }
 }
