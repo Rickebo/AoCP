09bb8829c6dd20b7ad59def9af90fbb40e3846b7
Jesper Wahl <jesper.wahl@hotmail.com>
2025-12-01 22:58:09 +0100

    Created files in advance for remaining days [2-12]

---
+++ src/backend/Backend/Problems/Year2025/batmanwarrior/Day12.cs
@@ -0,0 +1,71 @@
+using Common;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Backend.Problems.Year2025.batmanwarrior;
+
+public class Day12 : ProblemSet
+{
+    public override DateTime ReleaseTime { get; } = new(2025, 12, 12);
+
+    public override List<Problem> Problems { get; } =
+    [
+        new FirstProblem(),
+        new SecondProblem()
+    ];
+
+    public override string Name => "TITLE_DAY_12";
+
+    public class FirstProblem : Problem
+    {
+        public override string Name { get; } = "Part One";
+
+        public override string Description { get; } = "";
+
+        public override Task Solve(string input, Reporter reporter)
+        {
+            // Send solution to frontend (Part 1)
+            reporter.ReportSolution(new Solver(input, reporter, 1).PartOne());
+            return Task.CompletedTask;
+        }
+    }
+
+    public class SecondProblem : Problem
+    {
+        public override string Name { get; } = "Part Two";
+
+        public override string Description { get; } = "";
+
+        public override Task Solve(string input, Reporter reporter)
+        {
+            // Send solution to frontend (Part 2)
+            reporter.ReportSolution(new Solver(input, reporter, 2).PartTwo());
+            return Task.CompletedTask;
+        }
+    }
+
+    public class Solver
+    {
+        private readonly Reporter _reporter;
+
+        public Solver(string input, Reporter reporter, int _)
+        {
+            // Save for printing
+            _reporter = reporter;
+
+            // Parse input
+
+        }
+
+        public string PartOne()
+        {
+            return "";
+        }
+
+        public string PartTwo()
+        {
+            return "";
+        }
+    }
+}
