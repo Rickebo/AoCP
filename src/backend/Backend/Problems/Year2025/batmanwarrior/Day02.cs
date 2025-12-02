f6ee276406756c491c216583755bf38a3b14694a
Jesper Wahl <jesper.wahl@hotmail.com>
2025-12-02 09:12:58 +0100

    Added day 2 solution

--- src/backend/Backend/Problems/Year2025/batmanwarrior/Day02.cs
+++ src/backend/Backend/Problems/Year2025/batmanwarrior/Day02.cs
@@ -1,4 +1,5 @@
 using Common;
+using Lib.Parsing;
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
@@ -15,7 +16,7 @@ public class Day02 : ProblemSet
         new SecondProblem()
     ];
 
-    public override string Name => "TITLE_DAY_2";
+    public override string Name => "Gift Shop";
 
     public class FirstProblem : Problem
     {
@@ -48,24 +49,104 @@ public class Day02 : ProblemSet
     public class Solver
     {
         private readonly Reporter _reporter;
+        private readonly List<(ulong, ulong)> _ranges = [];
 
         public Solver(string input, Reporter reporter, int _)
         {
             // Save for printing
             _reporter = reporter;
 
-            // Parse input
-
+            // Parse input ranges
+            var values = Parser.GetValues<ulong>(input);
+            for (int i = 0; i < values.Length; i += 2)
+                _ranges.Add((values[i], values[i + 1]));
         }
 
-        public string PartOne()
+        public ulong PartOne()
         {
-            return "";
+            // Sum invalid IDs (two equal halves)
+            ulong sum = 0;
+            foreach (var (start, end) in _ranges)
+            {
+                for (ulong i = start; i <= end; i++)
+                {
+                    string str = i.ToString();
+                    int len = str.Length;
+
+                    // Must be even length
+                    if (len % 2 != 0)
+                        continue;
+
+                    // Check if halves are equal
+                    int half = len / 2;
+                    bool equal = true;
+                    for (int j = 0; j < half; j++)
+                    {
+                        if (str[j] != str[j + half])
+                        {
+                            equal = false;
+                            break;
+                        }
+                    }
+
+                    if (equal)
+                        sum += i;
+                }
+            }
+
+            return sum;
         }
 
-        public string PartTwo()
+        public ulong PartTwo()
         {
-            return "";
+            // Sum invalid IDs (all repeated chunks equal)
+            ulong sum = 0;
+            foreach (var range in _ranges)
+            {
+                for (ulong i = range.Item1; i <= range.Item2; i++)
+                {
+                    string str = i.ToString();
+                    int len = str.Length;
+
+                    // Skip single digit numbers
+                    if (len <= 1)
+                        continue;
+
+                    if (RepeatingSequence(str))
+                        sum += i;
+                }
+            }
+
+            return sum;
+        }
+
+        private static bool RepeatingSequence(string s)
+        {
+            int len = s.Length;
+
+            // Check pattern up to len / 2
+            for (int patternLen = 1; patternLen <= len / 2; patternLen++)
+            {
+                // Skip not evenly divided sequences
+                if (len % patternLen != 0)
+                    continue;
+
+                // Compare each character in sequence with pattern
+                bool allMatch = true;
+                for (int i = patternLen; i < len; i++)
+                {
+                    if (s[i] != s[i % patternLen])
+                    {
+                        allMatch = false;
+                        break;
+                    }
+                }
+
+                if (allMatch)
+                    return true;
+            }
+
+            return false;
         }
     }
 }
