244e1e69885d6bdaeffd13ac465e092c278cdc93
Jesper Wahl <jesper.wahl@hotmail.com>
2025-12-12 07:20:51 +0100

    Added day 12 "solution"

--- src/backend/Backend/Problems/Year2025/batmanwarrior/Day12.cs
+++ src/backend/Backend/Problems/Year2025/batmanwarrior/Day12.cs
@@ -1,6 +1,9 @@
 using Common;
+using Lib.Grids;
+using Lib.Text;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Backend.Problems.Year2025.batmanwarrior;
@@ -15,7 +18,7 @@ public class Day12 : ProblemSet
         new SecondProblem()
     ];
 
-    public override string Name => "TITLE_DAY_12";
+    public override string Name => "Christmas Tree Farm";
 
     public class FirstProblem : Problem
     {
@@ -48,6 +51,8 @@ public class Day12 : ProblemSet
     public class Solver
     {
         private readonly Reporter _reporter;
+        private readonly List<(CharGrid Grid, int Occupied, int Free)> _shapes = [];
+        private readonly List<(int Width, int Height, List<int> Quantities)> _regions = [];
 
         public Solver(string input, Reporter reporter, int _)
         {
@@ -55,18 +60,43 @@ public class Day12 : ProblemSet
             _reporter = reporter;
 
             // Parse input
+            string[] lines = input.SplitLines();
 
+            // Shapes
+            for (int i = 1; i < 24; i += 4)
+            {
+                CharGrid grid = new(string.Join('\n', lines[i..(i + 3)]));
+                int occupied = grid.FindAll(c => c == '#').Count();
+                int free = grid.Width * grid.Height - occupied;
+                _shapes.Add((grid, occupied, free));
+            }
+
+            // Regions
+            foreach (var line in lines[24..])
+            {
+                int[] vals = Parser.GetValues<int>(line);
+                _regions.Add((Width: vals[0], Height: vals[1], [..vals[2..]]));
+            }
         }
 
-        public string PartOne()
+        public int PartOne()
         {
-            return "";
+            int validRegions = 0;
+
+            foreach (var (Width, Height, Quantities) in _regions)
+            {
+                // Check if occupied present space fits in region (lucky & ugly 'solution')
+                int occupied = Quantities.Zip(_shapes, (q, shape) => q * shape.Occupied).Sum();
+                if (occupied <= Width * Height)
+                    validRegions++;
+            }
+            
+            return validRegions;
         }
 
         public string PartTwo()
         {
-            return "";
+            return "bruh";
         }
     }
 }
-
